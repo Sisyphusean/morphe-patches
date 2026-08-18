ca392e248e6dd2bef7e1dd4e77bff9edd3e7dba5
Rushi Ranpise <rushiranpise17@gmail.com>
2026-08-18 00:53:45 -0700

    fix: update monet

--- patches/src/main/kotlin/app/template/patches/shared/Constants.kt
+++ patches/src/main/kotlin/app/template/patches/shared/Constants.kt
@@ -1040,7 +1040,7 @@ val MONET_COMPATIBILITY = Compatibility(
         packageName = "com.klevico.monet",
         apkFileType = ApkFileType.APK,
         appIconColor = 0xFF6D00,
-        targets = listOf(AppTarget(version = "1.0.67", versionCode = 101))
+        targets = listOf(AppTarget(version = "1.0.73", versionCode = 107))
     )
 
 val MOVIEBOX_COMPATIBILITY = Compatibility(
